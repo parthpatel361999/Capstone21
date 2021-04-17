8b674c940ec5e1c18c91760782efb6fe8230192d
LostForestRunner <60615649+LostForestRunner@users.noreply.github.com>
2021-04-17 15:05:05 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,131 +1,50 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+import PIL
+from PIL import Image,ImageTk
+import pytesseract
+import cv2
 import cleaningObj
-import imutils
-import yaml
-from cleaningObj import *
-from cuboid import *
-from cuboid_pnp_solver import *
-from detector import *
-from handModel.handDetector import *
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
+import pickle
 
-# Constants
-[memSize_X, memSize_Y] = [500, 500]
-STOP_EXECUTION = "230495872345"
+from tkinter import *
+width, height = 900, 900
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-# Settings
-exposure_val = 166
+root = Tk()
+root.geometry('750x540')
+root.title("PROJECT TITLE")
+root.bind('<Escape>', lambda e: root.quit())
+def sclick():
+    filename = input("Enter file name")
+    savefile = open(filename, 'wb')
+    pickle.dump(lol, savefile)
+    print(lol.rightFace)
 
-# YAML Loader for DOPE
-yaml_path = 'webcam_config.yaml'
-with open(yaml_path, 'r') as stream:
-    try:
-        print("Loading DOPE parameters from '{}'...".format(yaml_path))
-        params = yaml.load(stream)
-        print('    Parameters loaded.')
-    except yaml.YAMLError as exc:
-        print(exc)
+def lclick():
+    filename2 = input("Enter file name")
+    readfile = open(filename2, 'rb')
+    loadobject = pickle.load(readfile)
+    cleaningObj.loadupdate(loadobject)
+    print(loadobject.rightFace)
 
-    models = {}
-    pnp_solvers = {}
-    pub_dimension = {}
-    draw_colors = {}
-    dimensions = {}
-    mainObjects = {}
-    # Initialize parameters
-    matrix_camera = np.zeros((3, 3))
-    matrix_camera[0, 0] = params["camera_settings"]['fx']
-    matrix_camera[1, 1] = params["camera_settings"]['fy']
-    matrix_camera[0, 2] = params["camera_settings"]['cx']
-    matrix_camera[1, 2] = params["camera_settings"]['cy']
-    matrix_camera[2, 2] = 1
-    dist_coeffs = np.zeros((4, 1))
+#lmain = Label(root,height=480, width=640)
+sbutton = Button(root, text="SAVE", height=2, width=5, command=sclick)
+sbutton.grid(column=1, row=0)
+lbutton = Button(root, text="LOAD", height=2, width=5, command=lclick)
+lbutton.grid(column=0, row=0)
+#lmain.grid(column=2, row=1)
 
-    if "dist_coeffs" in params["camera_settings"]:
-        dist_coeffs = np.array(params["camera_settings"]['dist_coeffs'])
-    config_detect = lambda: None
-    config_detect.mask_edges = 1
-    config_detect.mask_faces = 1
-    config_detect.vertex = 1
-    config_detect.threshold = 0.5
-    config_detect.softmax = 1000
-    config_detect.thresh_angle = params['thresh_angle']
-    config_detect.thresh_map = params['thresh_map']
-    config_detect.sigma = params['sigma']
-    config_detect.thresh_points = params["thresh_points"]
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = PIL.Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(10, show_frame)
 
-    # For each object to detect, load network model, create PNP solver, and start ROS publishers
-    for model in params['weights']:
-        models[model] = \
-            ModelData(
-                model,
-                "weights/" + params['weights'][model]
-            )
-        models[model].load_net_model()
-
-        draw_colors[model] = tuple(params["draw_colors"][model])
-
-        pnp_solvers[model] = \
-            CuboidPNPSolver(
-                model,
-                matrix_camera,
-                Cuboid3d(params['dimensions'][model]),
-                dist_coeffs=dist_coeffs
-            )
-        dimensions[model] = tuple(params['dimensions'][model])
-        mainObjects[model] = cleaningObj.CleaningObject(objId1=1, vertices1=np.zeros(8), quarternion1=0, color1=params['draw_colors'][model], dimensions=dimensions[model])
-detectHand_net = HandDetector()
-cap = WebcamVideoStream(src=0).start()
-frameCtrl = FPS().start()
-isHandDetected = False
-initialRun = True
-frameCount = 0
-frameThres = 10
-while True:
-    frame = cap.read()
-    frameDOPE = frame.copy()
-    frameDOPE = cv2.cvtColor(frameDOPE, cv2.COLOR_BGR2RGB)
-    frameHand = frame.copy()
-    cleaningMask = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)
-    if frameCount % frameThres == 0:
-        handCoords = detectHand_net.detectHand(frameHand)
-    for m in models:
-        if initialRun:
-            results = ObjectDetector.detect_object_in_image(models[m].net, pnp_solvers[m], frameDOPE, config_detect)
-            handCoords = None
-            initialRun = False
-        else:
-            if handCoords is None and (frameCount % frameThres == 0):
-                results = ObjectDetector.detect_object_in_image(models[m].net, pnp_solvers[m], frameDOPE, config_detect)
-        for i_r, result in enumerate(results):
-            if result["location"] is None:
-                continue
-            loc = result["location"]
-            ori = result["quaternion"]
-            score = result["score"]
-            if None not in result['projected_points']:
-                points2d = []
-                for pair in result['projected_points']:
-                    points2d.append(tuple(pair))
-
-                #testHand = [tuple([150, 150]), tuple([150, 250]), tuple([250, 250]), tuple([250, 150])]
-                cleaningMask = cleaningObj.drawCompleteMask(points2d, mainObjects[m], ori, score, handCoords, frame)
-            else:
-                print("NOTHING")
-    #img = cv2.cvtColor(frameDOPE, cv2.COLOR_RGB2BGR)
-    img = cv2.add(frame, cleaningMask)
-    if handCoords is not None and len(handCoords) >= 4:
-        cv2.rectangle(img, (handCoords[0],handCoords[1]),(handCoords[2],handCoords[3]),[0,0,255],3)
-    cv2.imshow('Cleaning Output', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    frameCtrl.update()
-    if frameCount % frameThres == 0:
-        frameCount = 0
-    frameCount+=1
-frameCtrl.stop()
-cap.stop()
-cv2.destroyAllWindows()
-
+lol = cleaningObj.CleaningObject([2,6,4],[7,6,2,3,6,4,6,4],[447,345],[37,37],[36,4,6])
+#show_frame()
+root.mainloop()
